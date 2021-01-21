d203fd7575c3707bc65d85975dd25bd8ed09ba83
veeduvv <vamsi.duvvuri@icloud.com>
2021-01-21 23:25:45 +0000

    successfully converted to tisits up to module 2

--- module-2/app/service/tisitService.py
+++ module-2/app/service/tisitService.py
@@ -10,18 +10,18 @@ CORS(app)
 # sent to the service root will receive a healthy response.
 @app.route("/")
 def healthCheckResponse():
-    return jsonify({"message" : "Nothing here, used for health check. Try /mysfits instead."})
+    return jsonify({"message" : "Nothing here, used for health check. Try /tisits instead."})
 
 # The main API resource that the next version of the Mythical Mysfits website
 # will utilize. It returns the data for all of the Mysfits to be displayed on
 # the website.  Because we do not yet have any persistent storage available for
 # our application, the mysfits are simply stored in a static JSON file. Which is
 # read from the the filesystem, and directly used as the service response.
-@app.route("/mysfits")
+@app.route("/tisits")
 def getMysfits():
 
     # read the mysfits JSON from the listed file.
-    response = Response(open("mysfits-response.json", "rb").read())
+    response = Response(open("tisit-response.json", "rb").read())
 
     # set the Content-Type header so that the browser is aware that the response
     # is formatted as JSON and our frontend JavaScript code is able to
